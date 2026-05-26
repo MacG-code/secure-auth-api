b4a8a918c2a02ca435913c8fea0fbacb66e98ba5
Mario Andres Cuevas <andrescuevasgm@gmail.com>
2026-05-26 17:31:51 -0500

    Add root redirect to Swagger docs

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView)
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('/api/docs/')
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     
